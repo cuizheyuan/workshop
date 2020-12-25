e3427d6e1094b95307fb6aef2207d616c0bfe7ec
randomCui <czy_scu@qq.com>
2020-12-25 22:51:04 +0800

    测试strcpy

--- string-exercise.c
+++ string-exercise.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#include<string.h>
 #define N 1024
 
+void strcp(char str[], int length);
 int func_length(char str[]);
 int main()
 {
@@ -10,6 +12,7 @@ int main()
 	length=func_length(str);
 	printf("%d",length);
 	return 0;
+    strcp(str, length);
 }
 int func_length(char str[])
 {
@@ -21,4 +24,11 @@ int func_length(char str[])
 		break;
 	}
 	return n;
+}
+
+void strcp(char str[],int length)
+{
+    char str_cp[1024] = {0};
+    strcpy(str_cp, str);
+    printf("%s", str_cp);
 }
