64f6ccb54110e060201ad64d220437749df7ae21
randomCui <czy_scu@qq.com>
2020-12-31 12:49:32 +0800

    修改了2000年不是闰年的bug

--- 2019test02.cpp
+++ 2019test02.cpp
@@ -1,16 +1,16 @@
 #include<stdio.h>
 
 int main(){
-	long type[2048]={1000000};
+	long type[128]={1000000};
 	int temp=0;
 	int counter=0;
 	int flag=0;
 		int n=0;
 		scanf("%d",&n);
-		getchar();;
-		for(int k=0;k<n;k++){
+		getchar();
+		for(int k=0;k<=n;k++){
 			scanf("%d",&temp);
-			for(int i=0;i<=counter;i++){
+			for(int i=0;i<=counter+1;i++){
 				if(temp==type[i]){
 				 flag=1;
 				}
--- temp.c
+++ temp.c
@@ -102,8 +102,12 @@ int is_run(int year)
 	if(year%4!=0)
 	return 0;
 	else{
-		if(year%100==0)
-		return 0;
+		if(year%100==0){
+			if(year%400==0)
+				return 1;
+			else
+				return 0;
+		}
 		else
 		return 1;
 	}
