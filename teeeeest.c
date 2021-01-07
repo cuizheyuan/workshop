4c5c19b0d30b0a735ab1e87734c28b8340f9b3c1
randomCui <czy_scu@qq.com>
2021-01-07 10:08:57 +0800

    数组名可以作为指针名

--- teeeeest.c
+++ teeeeest.c
@@ -7,5 +7,15 @@ int main()
     int m = 1;
     m += m * 3;
     printf("%d", m);
-    
+
+    int x[2][3] = {{1,2},{3,4},{5,6}};
+    printf("%d", m);
+
+    char a[10];
+    char b[7][8];
+    char *p1 = a + 3;
+    char *p2 = &b[2][4];
+    printf("%lld", p2-b[2]);
+    printf("%d", p2[9]-b[3][5]);
+
 }
