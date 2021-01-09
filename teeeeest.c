a9cb614cdaa170b8aa14cfa067eec57d619cae4f
randomCui <czy_scu@qq.com>
2021-01-09 22:59:24 +0800

    测试不同情况下指针的运行情况

--- teeeeest.c
+++ teeeeest.c
@@ -6,17 +6,17 @@ int main()
 {
     int m = 1;
     m += m * 3;
-    printf("%d", m);
+    printf("%d\n", m);
 
     int x[2][3] = {{1,2},{3,4},{5,6}};
-    printf("%d", m);
+    printf("%d\n", m);
 
     char a[10];
     char b[7][8];
     char *p1 = a + 3;
     char *p2 = &b[2][4];
-    printf("%lld", p2-b[2]);
-    printf("%d", p2[9]-b[3][5]);
+    printf("%lld\n", p2-b[2]);
+    printf("%d\n", p2[9]-b[3][5]);
     struct STRU
     {
         int a, b;
@@ -28,8 +28,9 @@ int main()
     int x1 = 1;
     int y1 = 2;
     int z1 = 3;
-    printf("%d", z1);
+    printf("%d\n", z1);
 
-    char arr[3][4] = {{"abcd"}, {"1234"},{"bomb"}};
-    printf("%s", *arr+2);
+    char arr[3][5] = {{"abcd"}, {"1234"},{"bomb"}};
+    printf("%s\n", *arr+2);
+
 }
