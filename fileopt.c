43ea03b6cceae82867eb282871a8146fe0abd4f9
randomCui <czy_scu@qq.com>
2021-01-13 13:40:54 +0800

    sprintf好难用啊啊啊啊

--- fileopt.c
+++ fileopt.c
@@ -2,11 +2,16 @@
 
 int main(int argc,char* argv[])
 {
-    FILE *fp;
-    char name[50];
-    char content[128];
-    scanf("%s", name);
-    fp = fopen(name, "r");
-    fgets(content, 128, fp);
-    fprintf(stdout, "%s", content);
+    //FILE *fp;
+    //char name[50];
+    //char content[128];
+    char in[50] = {0, 'a', 's',5,12,77,'l'};
+    int out[50] = {0};
+    //scanf("%s", name);
+    //fp = fopen(name, "r");
+    //fgets(content, 128, fp);
+    //fprintf(stdout, "%s", content);
+    for (int i = 0; i < 8;i++)
+        sprintf((char *)out, "%d", *in);
+    printf("%s ", (char*)out);
 }
